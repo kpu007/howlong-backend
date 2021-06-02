0ea640069bcaee06d5afe4303354e78557763ba0
kpu007 <kpuli007@fiu.edu>
2021-06-02 00:23:55 -0400

    Add endpoint to get all dates for a specific date

--- controllers/dates.js
+++ controllers/dates.js
@@ -1,14 +1,37 @@
 const dateRouter = require('express').Router()
-const Date = require('../models/date')
+const DateSchema = require('../models/date')
 
 dateRouter.get('/', async (request, response) => {
-  const dates = await Date.find({})
-  response.json(dates.map(product => product.toJSON()))
+  const dates = await DateSchema.find({})
+  response.json(dates.map(date => date.toJSON()))
 })
 
 dateRouter.get('/:name', async (request, response) => {
-  const dates = await Date.find({dateName: request.params.name})
-  response.json(dates.map(product => product.toJSON()))
+  const dates = await DateSchema.find({dateName: request.params.name})
+  response.json(dates.map(date => date.toJSON()))
+})
+
+dateRouter.get('/archive/:date', async (request, response) => {
+
+  const getBeginningOfDay = (date) => {
+    return date.setHours(0, 0, 0, 0).valueOf()
+  }
+
+  const extractDate = (date, translatedDate) => {
+    return {
+      dateName: date.dateName,
+      dateValue: date.dateArray.find(element => getBeginningOfDay(element.archivedDateTime) == translatedDate).archivedDateValue
+    }
+  }
+
+  //need to convert the parameter from a string to a number
+  const translatedDate = getBeginningOfDay(new Date(parseInt(request.params.date, 10)))
+  const dates = await DateSchema.find({})
+
+ 
+  const result = dates.map(date => extractDate(date, translatedDate))
+  console.log(result)
+  response.json(result)
 })
 
 module.exports = dateRouter
